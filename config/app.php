e6545e3c042ae90c3b6b8898d20a201c56901076
lamTD <32539012+lam-TD@users.noreply.github.com>
2018-08-24 09:56:54 +0700

    up

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => 'base64:/VstzATT1HmpsxqIxijCbB2qdVToU4bX9gR+wVuvLqY=',
 
     'cipher' => 'AES-256-CBC',
 
