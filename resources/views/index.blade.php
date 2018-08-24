482a0ddd3c15db2c87e4ec063410ff172b4960af
lamTD <32539012+lam-TD@users.noreply.github.com>
2018-08-24 10:04:40 +0700

    up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Đa cấp 2018</title>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <link href="{{asset('css/app.css')}}">
 
         {{--style--}}
         <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon.png">
